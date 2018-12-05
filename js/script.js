1535833c8041b16fefa7fb38077921f58d406cdf
Alex <shkolanumber4@gmail.com>
2018-12-04 22:11:28 -0500

    env setup

---
+++ js/script.js
@@ -0,0 +1,33 @@
+// // How to use jquery.
+
+
+// $(function () {
+//   $('h1').hide(500).show(500);
+//   $('h1').slideUp(1000).slideDown(1000);
+
+//   $('#main').css({
+//     color: "red",
+//     fontSize: 25
+//   });
+
+
+//   $('h1').click(function () {
+//     $('.content').css({
+//       color: "blue",
+//       fontSize: 50
+//     });
+//   });
+// });
+
+// $(window).on("load",function(){
+//   alert("Window is loaded.")
+// });
+
+
+/* ======================================================
+| | |             Preloader
+========================================================== */
+$(window).on('load', () => {
+  $('#status').fadeOut();
+  $('#preloader').delay(350).fadeOut();
+});
